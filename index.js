33d139a381f06de47f4bebb6e8bd976abf2a5039
houssein <nejihoussein1@gmail.com>
2023-05-16 18:46:08 +0000

    ebook backend first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express"
+import json_server from "json-server"
+import auth from "json-server-auth"
+const server = express()
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+})
+
+const router = json_server.router("./data/db.json")
+server.use("/api", router)
+server.db = router.db
+
+const middlewares = json_server.defaults()
+const rules = auth.rewriter({
+  products: 444,
+  featured_products: 444,
+  orderes: 600,
+  users: 600,
+})
+server.use(rules)
+server.use(auth)
+server.use(middlewares)
+server.use(router)
+server.listen(8000)
